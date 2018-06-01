b557081985393f23e218f87c50dcf1c2b1cfbf09
mikeh-elastic <michael.heldebrant@elastic.co>
2018-06-01 13:53:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var apm = require('elastic-apm-node').start({
   secretToken: '',
 
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: '10.128.0.3:8200'
+  serverUrl: 'http://10.128.0.3:8200'
 })
 //  OpenShift sample Node application
 var express = require('express'),
