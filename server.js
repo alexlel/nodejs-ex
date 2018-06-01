7a4cf7d8a5104507acf86eef138d73dbf0d29200
mikeh-elastic <michael.heldebrant@elastic.co>
2018-06-01 13:33:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+// Add this to the VERY top of the first file loaded in your app
+var apm = require('elastic-apm-node').start({
+  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+  serviceName: 'nodejs-ex',
+
+  // Use if APM Server requires a token
+  secretToken: '',
+
+  // Set custom APM Server URL (default: http://localhost:8200)
+  serverUrl: '10.128.0.3:8200'
+})
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
